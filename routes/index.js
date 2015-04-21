f8abc6ade8ac0e77fe868326a6dadb7a7f24c143
hyber1z0r <jakobsky@live.dk>
2015-04-21 20:12:31 +0200

    created server post route for sending notifications

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var API_KEY = 'AIzaSyCjMpbqlwrlkWg9DCkgulVzlaHOKOlr5Dc';
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/send', function (req, res) {
+    var data = {};
+    data.data = {};
+    // arg checking
+    data.registration_ids = ['APA91bENK1t7Ynkp8psaAMIXibQmdF5FWlOaomAPg1zYYJ66K8lg3GG2hl9VATgPM544KaBX4kudMfd2XLexMJLihZ0OrWTFm-Y_5pCbpkWrLVJry-ifr4ZMk-Nf3zP16go81rABRlyn1tQ2MbpZidI4YwCpkGl38A'];
+    data.data.string = 'this should work';
+
+    request.post({
+        url: 'https://android.googleapis.com/gcm/send',
+        body: JSON.stringify(data),
+        headers: {
+            'Authorization': 'key=' + API_KEY,
+            'Content-Type': 'application/json'
+        }
+    }, function callback(err, httpResponse, body) {
+        if (err) {
+            res.status(500).json({error: 'there was an error contacting the google cloud messesaging system, err: ' + err.message})
+        } else if (httpResponse.statusCode == 200) {
+            res.json({msg: 'success', googleResponse: body});
+        } else if (httpResponse.statusCode == 401) {
+            res.status(401).json({error: 'You are unauthorized'});
+        }
+    });
 });
 
 module.exports = router;
