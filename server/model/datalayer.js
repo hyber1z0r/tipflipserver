7ac6830b20ab9b94c57ed18c7350d6a01807d1a5
Filipovic <Damii93@hotmail.com>
2015-04-22 15:16:44 +0200

    Datalayer and reglayer is made

---
+++ server/model/datalayer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Filipovic on 22-04-2015.
+ */
+
+var reglayer = require ('./domain/reglayer');
+
+module.exports.saveRegIDs = reglayer.saveRegIDs;
+module.exports.getRegIds = reglayer.getRegIds;
